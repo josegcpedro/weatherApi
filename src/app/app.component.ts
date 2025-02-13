3249bef2259a331674dac331108fa8fda5a9da4a
Jose <pedrojcunha7@hotmail.com>
2025-02-13 21:01:02 +0100

    style: add video bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './services/weather.service';
-import { MatCardModule } from '@angular/material/card';
 import { CardComponent } from './components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatCardModule, CardComponent],
+  imports: [CommonModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -15,16 +15,19 @@ export class AppComponent {
   city: string = "";
   currentTemperature = 0;
   showTemperature: boolean = false;
+  time: string = "";
+  showTime: boolean = false;
 
   constructor(private weatherServices: WeatherService) { }
 
   getLocationWeather(city: string) {
-    this.city = city
-    console.log(city)
-    this.weatherServices.getWeather(city).subscribe((data) =>{
-      this.currentTemperature = data.current.temp_c
-      this.showTemperature = true;
-
+    this.city = city;
+    console.log(city);
+    this.weatherServices.getWeather(city).subscribe((data) => {
+    this.currentTemperature = data.current.temp_c;
+    this.time = data.current.last_updated
+    this.showTemperature = true;
+    this.showTime = true;
     })
   }
 }
